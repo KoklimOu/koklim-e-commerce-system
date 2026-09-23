36f8e2cf1fff1f81e64844cce7c0987c7770398a
koklim <oukoklim@gmail.com>
2026-09-23 15:33:20 +0700

    feat: validate customer & business for createOrderUseCase

--- order-service/order-service-persistence/src/main/java/kh/gov/mptc/koklim/ecommerce/order/persistence/adapter/OrderRepositoryAdapter.java
+++ order-service/order-service-persistence/src/main/java/kh/gov/mptc/koklim/ecommerce/order/persistence/adapter/OrderRepositoryAdapter.java
@@ -1,5 +1,7 @@
 package kh.gov.mptc.koklim.ecommerce.order.persistence.adapter;
 
+import kh.gov.mptc.koklim.ecommerce.order.application.port.output.BusinessRepository;
+import kh.gov.mptc.koklim.ecommerce.order.application.port.output.CustomerRepository;
 import kh.gov.mptc.koklim.ecommerce.order.domain.core.entity.Order;
 import kh.gov.mptc.koklim.ecommerce.order.persistence.repository.OrderJpaRepository;
 import kh.gov.mptc.koklim.ecommerce.order.application.dto.CreateOrderResult;
@@ -7,6 +9,7 @@ import kh.gov.mptc.koklim.ecommerce.order.application.port.output.OrderRepositor
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+/*A secondary adapter's only job is to save orders to the database. Business rules belong in the core.*/
 
 @Repository
 @RequiredArgsConstructor
@@ -16,7 +19,6 @@ public class OrderRepositoryAdapter implements OrderRepository {
 
     @Override
     public CreateOrderResult saveOrder(Order order) {
-        // Map Order to OrderEntity
         // Map OrderEntity to Order
         return null;
     }
